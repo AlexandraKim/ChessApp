b3ab21523e81f08c18473e69028d1a9e8ca1e7f7
Alexandra Kim <alexandra.success.kim@gmail.com>
2021-05-03 01:26:20 +0500

    transfer

---
+++ Scripts/dml-transfer.sql
@@ -0,0 +1,2 @@
+INSERT INTO Transfer (player_id, date, fee, new_federation_id, former_federation_id) values('3', '2016-10-10', 'null', 3, 1);
+INSERT INTO Transfer (player_id, date, fee, new_federation_id, former_federation_id) values('7', '2019-10-11', 'null', 1, 2);
