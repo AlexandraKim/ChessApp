ead80f403c6885cd3dfd258291f367cd7a83b734
Ulugbek Abirov <ulugbekabirov@gmail.com>
2021-05-03 02:13:46 +0500

    added visitor

---
+++ 12dml-visitor.sql
@@ -0,0 +1,4 @@
+﻿insert into Visitor(name, email, password) values('Abirov Ulugbek', 'ulugbekabirov@gmail.com', 'database123');
+
+insert into Visitor(name, email, password) values('Alexandra Kim', 'alexandrakim@gmail.com', 'ILoveChess3000');
+
