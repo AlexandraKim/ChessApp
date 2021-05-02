33b724154f8164325219e88211e056083b3725a1
Alexandra Kim <alexandra.success.kim@gmail.com>
2021-05-03 02:11:35 +0500

    scripts

---
+++ Scripts/8dml-tournament and organizer.sql
@@ -0,0 +1,26 @@
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(75, 'CIG 2021 - CP Asti U10', '2021-05-21', '2021-05-25');
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(28, '2021 Banff Open', '2021-11-12', '2021-11-17');
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(18, 'IRT "Fuego en el Tablero"', ' 2021-05-07', ' 2021-05-10');
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(18, 'Torneo IRT "Mario Dario Lopez"', '2021-04-30', '2021-04-31');
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(8, '2021 Rooty Hill Blitz Championship', '2021-12-14', '2021-12-18');
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(8, '2021 Sydney Chess Club December Sunday Blitz', '2021-12-05', '2021-12-07');
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(8, '2021 Rooty Hill Rapid Championship', '2021-11-23', '2021-11-25');
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(174, '12th Central Chess Club Sunday Open', '2021-05-08', '2021-05-10');
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(174, 'Zone 3.4 Zonal Open Championship', '2021-05-07', '2021-05-10');
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(174, '11th Central Chess Club Sunday Open', '2021-05-02', '2021-05-03');
+INSERT INTO Tournament (country_id, name, start_date, end_date) values(174, 'Uzbekistan Men Championship Final 2021', '2021-04-17', '2021-04-30');
+
+INSERT INTO Orginizer (name, website) values('Gazprom', 'https://www.gazprom.com/');
+INSERT INTO Orginizer (name, website) values('SWIPS', 'https://chess.swips.eu/');
+
+INSERT INTO Conducts (tournament_id, organizer_id) values(1, 1);
+INSERT INTO Conducts (tournament_id, organizer_id) values(2, 1);
+INSERT INTO Conducts (tournament_id, organizer_id) values(3, 1);
+INSERT INTO Conducts (tournament_id, organizer_id) values(4, 1);
+INSERT INTO Conducts (tournament_id, organizer_id) values(5, 1);
+INSERT INTO Conducts (tournament_id, organizer_id) values(6, 1);
+INSERT INTO Conducts (tournament_id, organizer_id) values(7, 1);
+INSERT INTO Conducts (tournament_id, organizer_id) values(8, 1);
+INSERT INTO Conducts (tournament_id, organizer_id) values(9, 2);
+INSERT INTO Conducts (tournament_id, organizer_id) values(10, 2);
+INSERT INTO Conducts (tournament_id, organizer_id) values(11, 2);
